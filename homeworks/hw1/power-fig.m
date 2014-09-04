a5ec32b05d8e81660594231dc1dae3f1da88cb03
Hershal Bhave <hershal.bhave@gmail.com>
2014-09-04 01:27:34 -0500

    removed crap

--- homeworks/hw1/power-fig.m
+++ homeworks/hw1/power-fig.m
@@ -1,22 +1,6 @@
 #!/usr/bin/env octave
 
 figure('visible', 'off');
-## ber1 = @(x) -real(besselj(1,x*exp(pi*1i/4)));
-## bei1 = @(x) imag(besselj(1,x*exp(1i*pi/4)));
-## delta = @(x) ber1(x)-bei1(x);
-## rho0 = fsolve(delta,4);
-## x=0:0.1:4;
-## plot(x,delta(x), 'linewidth' ,2);
-## hold on;
-## plot([rho0], [0],  'o',  'linewidth', 10);
-## text(rho0, 0.15,  '\colorbox{white}{$\rho_0$}',
-##      'horizontalalignment', 'center');
-## text(rho0, -0.2,
-##      sprintf("\\colorbox{white}{$%.2f$}", rho0),
-##      'horizontalalignment', 'center');
-## title ("");  legend ("off");  grid();
-## xlabel('$\rho$');
-## ylabel('$\text{ber}_1\rho-\text{bei}_1\rho$');
 
 x=0:.025:5;
 
